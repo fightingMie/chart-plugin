af19a0e511c0cca9739c1d1111d82227111094f5
liuyang29 <liuyang29@kingsoft.com>
2025-08-14 16:17:31 +0800

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,6 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -25,9 +22,9 @@ module.exports = {
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
-    'react/prop-types': 'off',
+    'no-undef': 'off', // TypeScript handles this
   },
   settings: {
     react: {
