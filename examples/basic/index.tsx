a4b05d9b263d927be77c8d26df84acefb75806a2
liuyang29 <liuyang29@kingsoft.com>
2025-08-14 18:26:36 +0800

    插件修复

--- examples/basic/index.tsx
+++ examples/basic/index.tsx
@@ -1,14 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { MetricChartPlugin } from 'react-metric-chart-plugin';
-interface MetricStats {
-  name: string;
-  current: number;
-  maximum: number;
-  minimum: number;
-  average: number;
-  color: string;
-}
+import { MetricChartPlugin, MetricStats } from 'react-metric-chart-plugin';
 import 'antd/dist/reset.css';
 
 const App: React.FC = () => {
@@ -131,7 +123,7 @@ const App: React.FC = () => {
   return (
     <div style={{ padding: 24 }}>
       <MetricChartPlugin
-        options={chartOptions}
+        chartOptions={chartOptions}
         statsData={statsData}
         title="系统资源监控"
         height={400}
